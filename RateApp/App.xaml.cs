0b71cb3cfca3a9e2a5782cb730606e0393a840c9
KALYAN <allam.kalyana@outlook.com>
2024-02-19 21:30:48 +0530

    Add project files.

---
+++ RateApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RateApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ RateApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RateApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
