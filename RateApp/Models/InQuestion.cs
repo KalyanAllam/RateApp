9dbc05bbf8726c894818a883ab01ef294b06b348
KALYAN <allam.kalyana@outlook.com>
2024-03-22 18:38:00 +0530

    Add db

---
+++ RateApp/Models/InQuestion.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateApp.Models
+{
+   public class InQuestion
+    {
+        [PrimaryKey, Indexed]
+        public int No { get; set; }
+
+            public int DaysBegin { get; set; }
+
+            public int DaysEnd { get; set; }
+
+            public decimal Interest { get; set; }
+
+            public decimal SrInterest { get; set; }
+
+       
+
+    }
+}
---
+++ RateApp/Models/QuestionsDatabase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+using RateApp.Models;
+
+
+namespace RateApp.Models
+{
+
+    
+ 
+        public class QuestionsDatabase
+        {
+            SQLiteAsyncConnection Database;
+            private SQLiteAsyncConnection _dbConnection;
+            public const string DatabaseFilename = "MCQ.db3";
+            async Task Init()
+            {
+                string dbPath =
+                 Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+                FileStream fileStream = new FileStream(dbPath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+
+                _dbConnection = new SQLiteAsyncConnection(dbPath);
+                if (Database is not null)
+                    return;
+                Database = new SQLiteAsyncConnection(dbPath, Constants.Flags);
+
+                var result = await Database.CreateTableAsync<InQuestion>();
+            }
+            public async Task<List<InQuestion>> GetItemsAsync()
+            {
+                await Init();
+                var res = await Database.Table<InQuestion>().ToListAsync();
+                return res;
+            }
+            public async Task<InQuestion> GetItemAsync(int id)
+            {
+                await Init();
+                return await Database.Table<InQuestion>().Where(i => i.No == id).FirstOrDefaultAsync();
+            }
+
+
+            public async Task<int> SaveItemAsync(InQuestion item)
+            {
+                await Init();
+                InQuestion res = await GetItemAsync(item.No);
+                if (res != null)
+                    return await Database.UpdateAsync(item);
+                else
+                    return await Database.InsertAsync(item);
+            }
+
+
+            public async Task<int> DeleteItemAsync(InQuestion item)
+            {
+                await Init();
+                return await Database.DeleteAsync(item);
+            }
+
+
+
+        }
+    }
+
+
+
+ 
